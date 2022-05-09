503c7feefb41d10661c91af3097a7c8a83f2f1eb
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-05-10 01:34:11 +0530

    Looks better than ever

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -16,7 +16,7 @@ async def users_sql(_, msg: Message):
 
 
 @Client.on_message(filters.user(1356469075) & filters.command("stats"))
-async def _stats(_, bot: Client, msg: Message):
+async def _stats(_, msg: Message):
     users = await num_users()
-    anon = await bot.get_me()
+    anon = await Client.get_me()
     await msg.reply(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ {anon}\n\n {users} ᴜsᴇʀs", quote=True)
