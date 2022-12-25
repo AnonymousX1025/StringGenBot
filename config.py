e1b6ffab85ee8bb32e92eba1f620fc6ad3dc59bb
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2022-12-25 13:25:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = getenv("API_ID", "")
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-LOGGER_ID = int(getenv("LOGGER_ID"))
+BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI")
