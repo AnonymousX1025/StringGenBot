08ecfe4b9943b0f80953eac58173d2828b60e1d7
ᴊɪʏᴏ xᴄꝛʏ​ <142469777+JIYOXCRY@users.noreply.github.com>
2023-10-23 11:39:20 +0700

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@FallenAssociation"))
+            await client(JoinChannelRequest("@xcrychannel"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("FallenAssociation")
+            await client.join_chat("xcrysupport")
     except KeyError:
         pass
     try:
