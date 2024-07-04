72262ddba7c594082fb504c9c78a5d1f65810be0
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-07-04 08:30:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 6848223695))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/about_tosuu")
